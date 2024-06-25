27658ae00391e0ac351c050cb37474d1a31feeef
mpc <matthewcoole@gmail.com>
2024-06-25 16:23:16 +0100

    Added basic RAG pipeline with accompanying API

---
+++ rag_api.py
@@ -0,0 +1,66 @@
+from haystack_integrations.document_stores.chroma import ChromaDocumentStore
+from haystack_integrations.components.retrievers.chroma import ChromaQueryTextRetriever
+from haystack.components.builders import PromptBuilder
+from haystack.components.generators import HuggingFaceLocalGenerator
+from haystack import Pipeline
+from typing import Union
+
+from fastapi import FastAPI
+
+
+def create_pipeline():
+    print("Setting up chroma db...")
+    document_store = ChromaDocumentStore(
+        collection_name="eidc-data", persist_path="haystack-chroma-data"
+    )
+    retriever = ChromaQueryTextRetriever(document_store, top_k=1)
+    print("Creating prompt template...")
+    template = """
+    Given the following information, answer the question.
+
+    Context:
+    {% for document in documents %}
+        {{ document.content }}
+    {% endfor %}
+
+    Question: {{query}}
+    Answer:
+    """
+    prompt_builder = PromptBuilder(template=template)
+    model_name = "google/flan-t5-large"
+    print(f"Setting up model ({model_name})...")
+    llm = HuggingFaceLocalGenerator(
+        model=model_name,
+        task="text2text-generation",
+        generation_kwargs={
+            "max_new_tokens": 100,
+            "temperature": 0.9,
+        },
+    )
+    print("Warming up model...")
+    llm.warm_up()
+    print("Creating haystack pipeline...")
+    rag_pipe = Pipeline()
+    rag_pipe.add_component("retriever", retriever)
+    rag_pipe.add_component("prompt_builder", prompt_builder)
+    rag_pipe.add_component("llm", llm)
+    rag_pipe.connect("retriever.documents", "prompt_builder.documents")
+    rag_pipe.connect("prompt_builder", "llm")
+    return rag_pipe
+
+
+pipeline = create_pipeline()
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/query")
+def query(query: Union[str, None] = None):
+    results = pipeline.run({"retriever": {"query": query, "top_k": 2},
+                        "prompt_builder": {"query": query},
+                        "llm":{"generation_kwargs": {"max_new_tokens": 350}}})
+    return {"query": query, "results": results}

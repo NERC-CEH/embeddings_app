c454d115bf6ea90a83c0117f1cbd4904cbcb0cd2
mpc <matthewcoole@gmail.com>
2024-06-28 15:30:31 +0100

    Cleaned code and removed cache files

--- tests/test_rag_app.py
+++ tests/test_rag_app.py
@@ -1,7 +1,7 @@
 from streamlit.testing.v1 import AppTest
 import requests
 from unittest import TestCase, mock
-
+import json
 
 class TestRagApp(TestCase):
 
@@ -13,9 +13,7 @@ class TestRagApp(TestCase):
         test_query = "Test question?"
         test_response = "This is a test answer."
         self.response.status_code = 200
-        self.response._content = (
-            '{"results": {"llm": {"replies": ["This is a test answer."]}}}'.encode()
-        )
+        self.response._content = json.dumps({"results": {"llm": {"replies": [test_response]}}}).encode()
         with mock.patch("requests.get", return_value=self.response):
             at.chat_input[0].set_value(test_query).run()
             assert at.chat_message[1].markdown[0].value == test_query

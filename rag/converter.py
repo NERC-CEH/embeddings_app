244284acd74f4d60d3512862191e9db1d4849c06
mpc <matthewcoole@gmail.com>
2024-08-02 15:42:41 +0100

    Extract all metadata fields unless a set is specified

--- rag/converter.py
+++ rag/converter.py
@@ -10,7 +10,11 @@ logger = logging.getLogger(__name__)
 @component
 class EIDCJSONToDocument:
     """
-    Converts a json response from the EIDC API to a set of Documents based on the results.
+    Converts a JSON response from the EIDC API to a set of Documents based on the results.
+
+    Each metadata field from the eidc will be individually converted to a document with referene to the dataset identifier and key name in the document metadata.
+
+    Alternatively, a list of fields can be specified to extract only the required metadata.
     """
 
     def __init__(self, metadata_fields: Optional[List[str]] = None):
@@ -30,11 +34,23 @@ class EIDCJSONToDocument:
                 )
 
             try:
-                json_string = bytestream.data.decode("utf-8")
-                api_response = json.loads(json_string)
+                api_response = json.loads(bytestream.data.decode("utf-8"))
+
                 for dataset in api_response["results"]:
-                    document = Document(content=dataset["title"])
-                    documents.append(document)
+                    keys = (
+                        self.metadata_fields
+                        if self.metadata_fields
+                        else dataset
+                    )
+                    for key in keys:
+                        metadata = {
+                            "src_dataset": dataset["identifier"],
+                            "eidc_metadata_key": key,
+                        }
+                        doc = Document(
+                            content=str(dataset[key]), meta=metadata
+                        )
+                        documents.append(doc)
 
             except Exception as conversion_e:
                 logger.warning(
